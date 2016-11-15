18851c70abbcf75add7162f1008704279dfd6b11
Eric <zamalchi@techsquare.com>
2016-11-15 13:13:25 -0500

    commenting out recursive git clone in centos7 installer

--- installers/centos7-installer.sh
+++ installers/centos7-installer.sh
@@ -8,7 +8,7 @@ sudo pip install --upgrade pip
 sudo pip install markdown
 sudo yum install pycrypto
 
-git clone https://www.github.com/zamalchi/hoursapp.git
+# git clone https://www.github.com/zamalchi/hoursapp.git
 
-cd hoursapp
-./launcher.sh
+# cd hoursapp
+../launcher.sh
