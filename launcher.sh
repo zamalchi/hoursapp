d19b577ea4bea09034381be8d174584161d63ee3
zamalchi <zamalchi@techsquare.com>
2016-11-17 16:56:44 -0500

    changed launcher back to python instead of python2.7

--- launcher.sh
+++ launcher.sh
@@ -4,6 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
 cd $parent_path
 
-export PYTHONPATH=$parent_path/src/packages:PYTHONPATH
-
-python2.7 ./run.py -p 8080
+python ./run.py -p 8080
