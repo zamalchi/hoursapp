b443c5350009bbdb7fa73f7e66416d0b500c656c
lawrence <lgardner462@hcc.edu>
2016-12-22 09:50:47 -0500

    changed root path to os.getcwd, works for linux and windows

--- larrytest/config/dirs.py
+++ larrytest/config/dirs.py
@@ -3,11 +3,7 @@
 import os
 import sys
 
-if sys.platform.startswith('win'):
-    ROOT_DIR = os.path.abspath('.')+"\\"
-elif sys.plataform.startswith('linux'):
-    ROOT_DIR = "/".join(os.path.abspath(__file__).split("/")[0:-2])
-    
+ROOT_DIR = os.getcwd()    
 #ROOT_DIR = "/".join(os.path.abspath(__file__).split("/")[0:-2])
 #ROOT_DIR = os.path.dirname(os.path.abspath('.'))
 #ROOT_DIR = os.path.abspath('.')+"\\"
